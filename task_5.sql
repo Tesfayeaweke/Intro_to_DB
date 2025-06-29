31e54911a6134a9b9cdf2cb00f902ba33fe28533
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-29 11:50:45 +0300

    inseted a row into customers table

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Active: 1751038883340@@127.0.0.1@3306@alx_book_store
+INSERT INTO Customers(customer_id,customer_name,email,address) 
+VALUES(1,'Cole Baidoo','cbaidoo@sandtech.com','123 Happiness Ave.');
