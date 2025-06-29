793c2ca95f44490774512d3b9db8705822ecf88d
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-29 08:58:43 +0300

    second correction made

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-CREATE Table Books(
+CREATE TABLE Books(
     book_id INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
@@ -8,23 +8,23 @@ CREATE Table Books(
     publication_date DATE,
     Foreign Key (author_id) REFERENCES Authors(author_id)
 );
-CREATE Table Authors(
+CREATE TABLE Authors(
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
-CREATE Table Customers(
+CREATE TABLE Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
-CREATE Table Orders(
+CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
     Foreign Key (customer_id) REFERENCES Customers(customer_id)
 );
-CREATE Table Order_Details(
+CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
