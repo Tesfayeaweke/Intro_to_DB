093df857e9b138aa427b5d9696f3b14faec2a868
Tesfaye Eshetu <tesfayee57@gmail.com>
2025-06-29 09:01:59 +0300

    fourth correction made.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,8 +28,8 @@ CREATE TABLE Order_Details(
     orderdetailid INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    Foreign Key (order_id) REFERENCES Orders(order_id),
-    Foreign Key (book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
 
